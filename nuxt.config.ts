189685e9a4f28d984df575a154052b0ac32d23c8
r-zyee-h <reezyeehan@gmail.com>
2025-03-30 21:54:08 +0700

    fixing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 export default defineNuxtConfig({
+  devtools: { enabled: false },
   css: ['@/assets/css/main.css'],
   modules: ['@nuxtjs/tailwindcss'],
   compatibilityDate: '2025-03-30',
+  nitro: {
+    preset: "netlify",
+  },
+  app: {
+    baseURL: "/", // Pastikan baseURL tidak kosong
+  }
 });
