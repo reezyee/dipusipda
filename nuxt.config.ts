a80c8e10007b940c965ae9a3ba85320fa75616a2
reezyee <rezasapitra514@gmail.com>
2025-04-04 18:54:25 +0700

    fixing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,23 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  css: ['@/assets/css/main.css'],
-  modules: ['@nuxtjs/tailwindcss'],
-  compatibilityDate: '2025-03-30',
-  nitro: {
-    preset: "vercel",
-  },
+  css: ["~/assets/css/main.css"], // CSS global
+  modules: ["@nuxtjs/tailwindcss"],
   app: {
-    baseURL: "/", // Pastikan baseURL tidak kosong
-  }
+    head: {
+      title: "Dipusipda Tasikmalaya Kota",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          name: "description",
+          content: "Website statis Dipusipda Tasikmalaya Kota",
+        },
+      ],
+      link: [{ rel: "icon", type: "image/x-icon", href: "logo.ico" }],
+    },
+  },
+  nitro: {
+    preset: "vercel", // Use the Vercel preset
+  },
 });
